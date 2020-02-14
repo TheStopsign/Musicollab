505a3768dd1fa62fd19862958b3cab4653460f7c
Ethan Whitton <ethanwhitton@snowmobility.com>
2020-02-14 15:27:18 -0500

    running on server.js instead of www

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@
 
 var express = require('express');
 var router = require('./routes/routes.js')
-var path = require('path'); var app = express();
+var path = require('path');
+var app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../client'));
@@ -10,5 +11,10 @@ app.set('views', path.join(__dirname, '../client'));
 
 app.use(express.static(path.join(__dirname, '../client')));
 app.use('/', router);
+
+var port = 8000
+app.listen(port, function () {
+	console.log('running at localhost: ' + port);
+});
 
 module.exports = app;
