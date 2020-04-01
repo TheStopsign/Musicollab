01d3948152c46ba1f3fac8e677d82c2f7710db18
dylanmoody <moodyd2@rpi.edu>
2020-04-01 17:45:16 -0400

    fixed spacing issues, stuck footer to bottom of page, created new root class for easy consistent style accross website

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,43 +3,41 @@
 import React, { Component } from 'react';
 import '../App.css';
 import { Link } from "react-router-dom";
+import Logo from '../logo.svg';
 
 class Profile extends Component {
 	render() {
 		return (
 			<div className="Profile">
-				<div className="container-fluid">
 
-					<div className="row align-items-center head section">
-						<div className="col-3">
-							<a href="/home" className="svg">
-								<object type="image/svg+xml" data="../logo.svg" height="80"></object>
-							</a>
-						</div>
-
-						<div className="col-6 searchBar">
-							<input type="text" id="search" name="search" placeholder="Search" />
-						</div>
-
-						<div className="col-1">
-							<img className="float-right pic" src="profile.jpg" alt="prfile picture" />
-						</div>
-
-						<div className="col-2 user">
-							<ul>
-								<li><a href="profile"> Username</a></li>
-								<li className="userID">User ID</li>
-							</ul>
-						</div>
+				<div className="row align-items-center head section">
+					<div className="col-3">
+						<a href="/home" className="svg">
+							<img src={Logo} alt="Logo" height="80px" />
+						</a>
 					</div>
 
-				</div>
+					<div className="col-6 searchBar">
+						<input type="text" id="search" name="search" placeholder="Search" />
+					</div>
 
+					<div className="col-1">
+						<img className="float-right pic" src="profile.jpg" alt="prfile picture" />
+					</div>
+
+					<div className="col-2 user">
+						<ul>
+							<li><a href="/profile"> Username</a></li>
+							<li className="userID">User ID</li>
+						</ul>
+					</div>
+				</div>
+				<div className="main container-fluid">
+
+				</div>
 				<div className="container-fluid">
-					<div className="row footer">
-						<div className="col">
+					<div className="row footer section">
 							Musicollab is a 2020 SD&D project
-            </div>
 					</div>
 				</div>
 			</div>
