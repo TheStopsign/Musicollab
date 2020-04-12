1b512b58145da073eec78bb4cbac0cacde2a4d7c
Ethan Whitton <ethanwhitton@snowmobility.com>
2020-04-12 17:37:07 -0400

    new note state called dots, and dots rendering based on value

--- src/components/Note.js
+++ src/components/Note.js
@@ -10,17 +10,33 @@ class Note extends Component {
 			note: props.note,
 			offset: 0,
 			measure: props.measure,
-			location: props.location
+			location: props.location,
+			dots: 0 || props.dots
 		}
 	}
 	componentDidMount() {
-		this.setHeight()
+
 	}
 	setNote(newnote) {
 		this.setState({ note: newnote })
 	}
 	getNote() {
 		return this.state.note
+	}
+	getDots() {
+		let dots = [];
+		for (var i = 0; i < this.state.dots; i++) {
+			if (i == 0) {
+				dots.push(
+					<div className="dot first" />
+				);
+			} else {
+				dots.push(
+					<div className="dot additional" />
+				);
+			}
+		}
+		return dots;
 	}
 }
 
